e796647de4ef617d1827d53ce9cd5532e057b833
savioferrazgit config --global user.name savioferrazclearclsgit config --global user.name savioferrazgit config --global user.name savioferraz <savio.ferraz88@gmail.com>
2022-06-19 21:43:11 -0300

    Versão 0.3

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
-let qtdcartas = 8;
+let qtdcartas = 14;
 let figurascartas = [
     "bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"
 ];
-let temCartaVirada = false;
-let primeiraCarta;
-let segundaCarta;
+let jogadas = 0;
+let acertos = 0;
+var bloquear = false;
+var temCartaVirada = false;
+var primeiraCarta;
+var segundaCarta;
 
 // iniciar();
 // function iniciar(){
@@ -17,26 +20,50 @@ let segundaCarta;
 for (let i = 0; i < (qtdcartas / 2); i++){
     const cartasTemplate = `
      <div class="cartaLayout" onclick="virar(this)">
-        <div class="cartaConteudo" data-figura="${figurascartas[i]}">
+        <div class="cartaConteudo" style="order: ${i}" data-figura="${figurascartas[i]}">
             <div class="frente"><img src="./images/front.png" style="width:100px;height:100px";></div>
             <div class="verso"><img src="./images/${figurascartas[i]}.gif" style="width:100px;height:100px"></div>
         </div> 
    </div>`;
 document.querySelector(".tabuleiro").innerHTML += cartasTemplate;
+}
+
+for (let i = (qtdcartas / 2); i < qtdcartas; i++){
+    const cartasTemplate = `
+     <div class="cartaLayout" onclick="virar(this)">
+        <div class="cartaConteudo" style="order: ${i}" data-figura="${figurascartas[i-(qtdcartas/2)]}">
+            <div class="frente"><img src="./images/front.png" style="width:100px;height:100px";></div>
+            <div class="verso"><img src="./images/${figurascartas[i-(qtdcartas/2)]}.gif" style="width:100px;height:100px"></div>
+        </div> 
+   </div>`;
 document.querySelector(".tabuleiro").innerHTML += cartasTemplate;
 }
 
+cartasNode = document.querySelectorAll(".cartaLayout");
+const cartasArray = Array.from(cartasNode);
+
+cartasArray.sort(embaralhar);
+
+function embaralhar() {
+    return Math.random() - 0.5;
+}
+
 function virar(elemento) {
+    if (bloquear) return;
+    if (this === primeiraCarta) return;
     cartaVirada = elemento.querySelector(".cartaConteudo");
     cartaVirada.classList.add("virado");
     if (!temCartaVirada) {
         temCartaVirada = true;
         primeiraCarta = cartaVirada;
+        jogadas ++;
     }
     else if (temCartaVirada) {
         segundaCarta = cartaVirada;
+        jogadas ++;
+        setTimeout(checarPar, 1000);
+        return;
     }
-    setTimeout(checarPar, 1000);
 }
 
 function checarPar() {
@@ -49,9 +76,19 @@ function checarPar() {
 function removerCartas() {
     primeiraCarta.classList.add("removido");
     segundaCarta.classList.add("removido");
+    temCartaVirada = false;
+    primeiraCarta = undefined;
+    segundaCarta = undefined;
+    acertos ++;
+    if (acertos === (qtdcartas/2)) {
+        alert (`Parabéns! Você venceu com ${jogadas/2} jogadas!`);
+    }
 }
 
 function desvirarCartas(){
     primeiraCarta.classList.remove("virado");
     segundaCarta.classList.remove("virado");
+    temCartaVirada = false;
+    primeiraCarta = undefined;
+    segundaCarta = undefined;
 }
